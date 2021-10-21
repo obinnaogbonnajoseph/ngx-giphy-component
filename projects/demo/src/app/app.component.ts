02a79974ef7d9d3791106f54efa9504413ccfdc0
Obinna Ogbonna <oogbonna@gidimobile.com>
2021-10-21 21:38:41 +0100

    update package json ng-package.json

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+export interface SearchVal {
+  searchText: string;
+  reset: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+  gifs: any[] = [];
+  loading: boolean = false;
+  mode: 'stickers' | 'gifs' = 'gifs';
+
+  searchObj: SearchVal = {
+    searchText: '',
+    reset: true
+  }
+
+  setGifs(gifs: any[]): void {
+    this.gifs = gifs;
+  }
+
+  getUrl(url: string): string {
+    return '';
+  }
+
+  selectGif(gif: any, index: number): void { }
+
+  searchGif(val: string) {
+    this.searchObj = {
+      searchText: val,
+      reset: true
+    };
+  }
+
+  setLoading(loading: boolean) {
+    this.loading = loading;
+  }
+
+  changeMode(mode: 'gifs' | 'stickers'): void {
+    this.mode = mode;
+  }
 }
