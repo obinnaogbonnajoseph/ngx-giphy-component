96212d44232d459c15c0f1c2cf4c917046193afe
Obinna Ogbonna <oogbonna@gidimobile.com>
2021-10-29 09:50:56 +0100

    fix search

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -30,10 +30,9 @@ export class AppComponent {
 
   selectGif(gif: any, index: number): void { }
 
-  searchGif(val: any) {
-    console.log('*** val ***')
+  searchGif(target: any) {
     this.searchObj = {
-      searchText: val,
+      searchText: target.value,
       reset: true
     };
   }
